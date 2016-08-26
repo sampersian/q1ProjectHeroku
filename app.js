cafbfd9f05c82557a5346645c9e9c43c1270647f
Samuel Persian <samuel.persian@gmail.com>
2016-08-26 11:49:18 -0600

    why wont cors work

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-app.use(cors());
+app.use(cors);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
