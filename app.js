0a951cef528b8c01d8880e55151f32bdbd6912db
Samuel Persian <samuel.persian@gmail.com>
2016-08-26 11:47:16 -0600

    added the correct style sheet

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 "use strict";
-var express = require('express');
+var express = require('cors-express');
 var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
